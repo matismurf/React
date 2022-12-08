82ba1182c24b4280a10f26ff6f79ee777b01f0d7
Matias K76 <55673958+matismurf@users.noreply.github.com>
2022-12-08 09:32:17 -0300

    Versão do Projeto 1 Ignite com TypeScript

---
+++ IgniteComTypescript/src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps{
+    content: string;
+    deleteComment: (comment: string) => void;
+}
+
+export function Comment({content, deleteComment}:CommentProps){
+
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment(){
+
+        deleteComment(content)
+    }
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1)
+    }
+
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/matismurf.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Barack Obama</strong>
+                            <time>Cerca de 1 hora atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
