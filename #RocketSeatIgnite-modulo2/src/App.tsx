cad5758d28fe7f6f92ff624858f977afc6fe4e95
Matias K76 <55673958+matismurf@users.noreply.github.com>
2022-12-26 11:26:33 -0300

    Add files via upload

--- #RocketSeatIgnite-modulo2/src/App.tsx
+++ #RocketSeatIgnite-modulo2/src/App.tsx
@@ -4,12 +4,19 @@ import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
 import { Router } from './Router'
 import {BrowserRouter} from 'react-router-dom'
+import { createContext } from 'react'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+
 
 export function App() {
+  
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle/>
     </ThemeProvider>
--- #RocketSeatIgnite-modulo2/src/pages/History/index.tsx
+++ #RocketSeatIgnite-modulo2/src/pages/History/index.tsx
@@ -1,7 +1,11 @@
-import { Header } from "../../components/Header";
+import { useContext } from "react";
+import ptBR from 'date-fns/locale/pt-BR'
+import { formatDistanceToNow } from "date-fns";
+import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History(){
+    const {cycles}=useContext(CyclesContext)
     return(
         <HistoryContainer>
             <h1>Meu histórico</h1>
@@ -17,36 +21,20 @@ export function History(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="yellow">Em andamento</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="red">Interrompido</Status></td>
-                        </tr>
+                        {cycles.map(cycle=>{
+                            return(
+                                <tr key={cycle.id}>
+                                    <td>{cycle.task}</td>
+                                    <td>{cycle.minutesAmount} minutos</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {addSuffix:true, locale:ptBR})}</td>
+                                    <td>
+                                        {cycle.finishedDate && (<Status statusColor="green">Concluído</Status>)}
+                                        {cycle.interruptedDate && (<Status statusColor="red">Interrompido</Status>)}
+                                        {(!cycle.finishedDate && !cycle.interruptedDate) && (<Status statusColor="yellow">Em andamento</Status>)}
+                                    </td>
+                                </tr>
+                            )
+                            })}
                     </tbody>
                 </table>
             </HistoryList>
